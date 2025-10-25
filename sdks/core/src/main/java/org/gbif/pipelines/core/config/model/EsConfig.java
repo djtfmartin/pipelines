8715bda52267c2630eb47173f900fae0f97260e8
Dave Martin <djtfmartin@gmail.com>
2025-10-25 14:03:03 +0100

    WIP - tracking disabled

---
+++ sdks/core/src/main/java/org/gbif/pipelines/core/config/model/EsConfig.java
@@ -0,0 +1,29 @@
+package org.gbif.pipelines.core.config.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EsConfig implements Serializable {
+
+  private static final long serialVersionUID = -2392370864481517738L;
+
+  private String esHosts;
+    private String esSchemaPath;
+    private String esAlias;
+    private String indexRefreshInterval;
+    private String indexMaxResultWindow;
+    private String unassignedNodeDelay;
+    private String useSlowlog;
+    private String indexSearchSlowlogThresholdQueryWarn;
+    private String indexSearchSlowlogThresholdQueryInfo;
+    private String indexSearchSlowlogThresholdFetchWarn;
+    private String indexSearchSlowlogThresholdFetchInfo;
+    private String indexSearchSlowlogLevel;
+    private String searchQueryTimeoutSec;
+    private String searchQueryAttempts;
+}
